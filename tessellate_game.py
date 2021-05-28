969876918d06256993158d628158dadeea407160
mcembalest <mcembalest@g.harvard.edu>
2021-05-28 00:28:08 -0400

    started RL training

---
+++ tessellate_game.py
@@ -0,0 +1,114 @@
+from sys import maxsize as inf
+import time
+import numpy as np
+
+#constants to represent each game piece
+RED, BLUE, EMPTY, OUT_OF_PLAY = 0, 1, 2, 3
+
+
+class GameState:
+	# Class to handle the game logic
+
+	def __init__(self, against_computer = False):
+		# the game board is a 10x10 matrix
+		# possible values for entries in the game board are EMPTY, RED, BLUE, OUT_OF_PLAY
+		self.board = np.array([[EMPTY for i in range(10)] for j in range(10)])
+
+		# switches between RED := 0, BLUE := 1
+		self.turn = RED
+
+		self.score = {RED : 1, BLUE : 1}
+		self.num_islands = {RED : 0, BLUE : 0}
+
+		self.legal_moves = {(i, j) : True for i in range(10) for j in range(10)}
+
+	def new_move(self, row, col):
+
+		if not self.playable(row, col):
+			print('uh oh', row, col)
+			print(self.board)
+			print('legal', [x for x in self.legal_moves if self.legal_moves[x]==1])
+			raise ValueError('ILLEGAL MOVE')
+
+		# add the tile to the board
+		self.add_tile(row, col)
+
+		# switch the current player
+		self.turn = 1 - self.turn
+
+		self.calculate_score()
+
+	def add_tile(self, row, col):
+		# add a tile to the board, and prevent invalid tiles from being clicked on in the future
+
+		self.board[row][col] = self.turn
+		self.board[row][col + (-1)**col] = OUT_OF_PLAY
+		self.board[row + (-1)**row][col] = OUT_OF_PLAY
+
+		self.legal_moves[(row, col)] = 0
+		self.legal_moves[(row, col + (-1)**col)] = 0
+		self.legal_moves[(row + (-1)**row, col)] = 0
+
+	# def unmake_move(self, row, col):
+	# 	# make the space at (row, col) set to the value EMPTY
+	# 	# also, reverse any tiles that would have been labelled OUT_OF_PLAY by the tile at (row, col)
+	# 	self.board[row][col] = EMPTY
+	# 	self.legal_moves[(row, col)] = 1
+	# 	if self.board[row + (-1)**row][col + (-1 )**col] == EMPTY:
+	# 		self.board[row][col + (-1)**col] = EMPTY
+	# 		self.board[row + (-1)**row][col] = EMPTY
+
+	# 		self.legal_moves[(row, col + (-1)**col)] = 1
+	# 		self.legal_moves[(row + (-1)**row, col)] = 1
+	# 	self.turn = 1 - self.turn
+
+	def calculate_score(self):
+		# Returns the score for each player
+		# A player's score is defined as the product of all their current island sizes
+		# This function uses dfs(...) to determine the size of each island
+
+		self.score = {RED : 1, BLUE : 1}
+		self.num_islands = {RED : 0, BLUE : 0}
+		visited = [[False for j in range(10)] for i in range(10)]
+		for row0 in range(10):
+			for col0 in range(10):
+				color = self.board[row0][col0]
+				if color in [RED, BLUE] and not visited[row0][col0]:
+					size = self.dfs(row0, col0, color, visited)
+					self.score[color] *= size
+					if size > 1:
+						self.num_islands[color] += 1
+
+	def get_score(self):
+		return self.score[RED], self.score[BLUE]
+
+	def dfs(self, row0, col0, color, visited):
+		# helper function for calculate_score
+		# Returns the number of reachable nodes from starting point (row0, col0) using depth-first search
+
+		size = 0
+		stack = [(row0, col0)]
+		while stack:
+			row, col = stack.pop(0)
+			if not visited[row][col] and self.board[row][col] == color:
+				size += 1
+				visited[row][col] = True
+				stack += self.neighbors(row, col)
+		return size
+
+	def neighbors(self, row, col):
+		#Returns a list of the coordinates of the neighbors of (row, col)
+
+		#the (row, col) coordinates of any neighboring piece
+		#sorry for the annoyingly long formula...think of it as a challenge to see for yourself why it works :D
+		neighbor_coords = [(row + (-1)**row, col + (-1 )**col), (row - 1, col - (-1)**(row + col + 1)), (row + 1, col + (-1)**(row + col + 1)), (row + (-1)**(row + 1), col), (row, col + (-1)**(col + 1))]
+		
+		#use filter to remove any neighboring coordinates that fall off the board
+		return list(filter(lambda p : p[0] in range(10) and p[1] in range(10), neighbor_coords))
+
+	def playable(self, row, col):
+		# Returns True if and only if there is no tile currently at (row, col)
+		return self.board[row][col] == EMPTY
+
+
+	
---
+++ tessellate_window.py
@@ -0,0 +1,222 @@
+import tkinter as tk
+import time
+from tessellate_game import GameState, RED, BLUE
+from tessellate_train import get_action
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+
+
+PREVIEW, NO_PREVIEW = 0, 1
+
+#formatting constants
+bg_color = 'grey'
+bg_tag = 'background'
+board_outline_color = '#0de50d'
+color_map = {RED : {False : 'red', True : '#F87272'}, BLUE : {False : 'blue', True : '#94A8F8'}}
+num_squares = 5
+line_width = 15
+window_size = 600
+sq_size = int(window_size/num_squares)
+
+
+class GameWindow(tk.Tk):
+	# Class to handle the tkinter logic of drawing shapes and handling the mouse behavior
+	
+	def __init__(self, against_computer = False):
+		#initializes the GameWindow class (inheriting from the tkinter base class)
+		tk.Tk.__init__(self)
+
+		self.setup_canvas()
+
+		# boolean variable, controls whether the game should draw a temporary tile, to simulate the mouse "hovering" over the board
+		# depending on the current coordinates of the mouse
+		# the design is intended to only display the preview in locations on the board where location of the user's intended click is unambiguous
+		self.display_state = NO_PREVIEW
+
+		# stores a reference reference to the tile being current displayed while the mouse is hovering
+		self.current_display = None
+
+		# stores the board information as defined by the GameState class above
+		self.game_state = GameState(against_computer)
+
+		if against_computer:
+			self.p2model = keras.models.load_model('p2model', compile = False)
+
+		self.against_computer = against_computer
+		self.show_saliency = show_saliency
+
+
+		self.draw_base()
+		
+	def setup_canvas(self):
+		#Formats the tkinter canvas
+
+		#add label to displays the scores at the top
+		self.score_label = tk.Label(text = 'red : 1     blue : 1', font = ("Helvetica", 20))
+		self.score_label.pack()
+
+		#create Canvas where we draw the game board
+		self.canvas = tk.Canvas(height = window_size, width = window_size)
+		self.canvas.pack()
+
+		#equip canvas with a mouse listener, calls the function respond_to_click below
+		self.canvas.tag_bind(bg_tag, '<Button-1>', self.respond_to_click)
+		self.bind('<Motion>', self.motion)
+	
+	def motion(self,event):
+		# Updates the game and display information whenever the user moves their mouse
+
+		# When the user is hovering over a location they are allowed to play on, a preview of the move is displayed to the board
+		# Then, that preview tile is deleted from the canvas when the user moves their mouse away to a new spot
+
+		#gets the coordinate the user clicked at
+		result = self.clickable_coord(event.x, event.y)
+
+		if result:
+			row, col = result
+
+			if self.display_state == NO_PREVIEW:
+
+				self.display_state = PREVIEW
+				self.current_display = result
+				self.temp_tile = self.draw_tile(row, col, preview = True)
+
+			elif self.display_state == PREVIEW and self.current_display != result:
+
+				self.canvas.delete(self.temp_tile)
+				self.current_display = result
+				self.temp_tile = self.draw_tile(row, col, preview = True)
+
+		elif self.display_state == PREVIEW:
+			self.canvas.delete(self.temp_tile)
+			self.display_state = NO_PREVIEW
+
+
+	def respond_to_click(self, event):
+		# Updates the window and game information whenever the user clicks their mouse
+
+		result = self.clickable_coord(event.x, event.y)
+
+		if result:
+
+			#get the coordinates where the user clicked
+			row, col = result
+
+			#draw tile on the board
+			self.draw_tile(row, col)
+
+			# update the information in the GameState object
+			self.game_state.new_move(row, col)
+
+			#get the current score
+			red_score, blue_score = self.game_state.get_score()
+
+			#format current score onto the window
+			self.score_label['text'] = "red : {}     blue : {}".format(red_score, blue_score)
+
+
+			# handle computer playing a turn if playing against the computer
+
+			if self.against_computer:
+
+
+
+				# wait a few seconds
+				time.sleep(.5)
+
+				#observation = np.array(self.game_state.board)
+				epsilon = 0.9
+				action = get_action(self.game_state,self.p2model,0)
+				action = int(action)
+				opp_row, opp_col = action//10, action%10
+				if not self.game_state.playable(opp_row, opp_col):
+					raise ValueError('PLAYER 2 PICKED AN ILLEGAL MOVE')
+
+				# # opponent makes a move
+				# opp_row, opp_col = self.game_state.find_best_opponent_move()
+
+				self.draw_tile(opp_row, opp_col)
+
+				# update the information in the GameState object
+				self.game_state.new_move(opp_row, opp_col)
+
+				#get the current score
+				red_score, blue_score = self.game_state.get_score()
+
+				#format current score onto the window
+				self.score_label['text'] = "red : {}     blue : {}".format(red_score, blue_score)
+
+			self.draw_saliency()
+
+
+							#self.canvas.create_oval(60,60,210,210)
+					
+
+	def clickable_coord(self, x, y):
+		# Returns the grid coordinate where the user clicked, but only if the row is clickable
+
+		# a location (x, y) is clickable if it passes the following 3 checks
+
+		# 1) the user has clicked on the game board
+		# 2) the user has clicked near a corner of a grid square
+		# 3) there is currently no existing game piece where the user has clicked
+
+		# check 1) the user has clicked on the game board
+		off_the_board = (x < 0 or x >= window_size or y < 0 or y >= window_size)
+		if not off_the_board:
+
+			# check 2) the user has clicked near a corner of a grid square
+			xx, yy = x % sq_size, y % sq_size
+			in_a_corner = not ((xx > sq_size//3 and xx < 2*sq_size//3) or (yy > sq_size//3 and yy < 2*sq_size//3))
+			if in_a_corner:
+
+				# check 3) there is currently no existing game piece where the user has clicked
+				row, col = y // (window_size // 10), x // (window_size // 10)
+
+				if self.game_state.playable(row, col):
+
+					return (row, col)
+	
+	
+	def draw_base(self):
+		# draws underlying board
+
+		self.canvas.create_rectangle(0, 0, window_size, window_size, fill = bg_color, tag = bg_tag) #background
+		for i in range(1, num_squares): #white lines
+			self.canvas.create_line(i*sq_size, line_width, i*sq_size, window_size - line_width, width = 5, fill = 'white')
+			self.canvas.create_line(line_width, i*sq_size, window_size - line_width, i*sq_size, width = 5, fill = 'white')
+
+	def draw_tile(self, row, col, preview = False):
+		# draws a triangular tile onto the board based on the row, col of the users move
+		# preview: boolean variable that tells whether the tile is a move, or just the preview of a move
+		# if preview == True, use lighter color, else use darker color to draw tile
+
+		# formula to convert the row, col coordinates into the coordinates of its cooresponding triangle on the board
+		x, xx, y, yy = col//2, col % 2, row//2, row % 2
+		points = [p*sq_size for p in [x + xx, y + yy, x + 1 - xx, y + yy, x + xx, y + 1 - yy]]
+
+
+		return self.canvas.create_polygon(points, outline = bg_color, fill = color_map[self.game_state.turn][preview], tag = bg_tag)
+
+	# def draw_saliency(self):
+	# 	if self.show_saliency:
+	# 		print('drawing saliency')
+	# 		logits = self.p2model.predict(self.game_state.board.reshape(1,10,10,1))
+	# 		prob_weights = tf.nn.softmax(logits).numpy().flatten()
+	# 		playable_spots = np.array([self.game_state.legal_moves[(i//10, i%10)] for i in range(100)])
+	# 		masked_prob_weights = (prob_weights * playable_spots).reshape(10,10)
+	# 		max_val = np.max(masked_prob_weights)
+
+	# 		for row in range(10):
+	# 			for col in range(10):
+	# 				c_ = masked_prob_weights[row, col]
+	# 				x, xx, y, yy = col//2, col % 2, row//2, row % 2
+	# 				points = [p*sq_size for p in [x + xx, y + yy, x + 1 - xx, y + yy, x + xx, y + 1 - yy]]
+
+	# 				circ_x = np.mean([points[0], points[2], points[4]])
+	# 				circ_y = np.mean([points[1], points[3], points[5]])
+	# 				r = 50
+	# 				self.canvas.create_oval(circ_x - r, circ_y - r, circ_x + r, circ_y + r, fill = 'green', alpha = c_/max_val)
+
